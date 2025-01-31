53af7bbfb3f042994c647d6c27ce8fa83ebb72c5
Joshwa Sanz <joshwasanz@gmail.com>
2025-01-31 11:23:34 -0500

    templates, admin, class view setup

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import TaskList
 
 urlpatterns = [
-    path("",view=views.taskList,name="tasks")
+    path("",view=TaskList.as_view(),name="tasks")
 ]
