9385c92efbe790859ce19837f25ee27bff70ba53
Joshwa Sanz <joshwasanz@gmail.com>
2025-01-31 13:08:16 -0500

    create view implemented

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList,TaskDetail,TaskCreate
 
 urlpatterns = [
-    path("",view=TaskList.as_view(),name="tasks")
+    path("",view=TaskList.as_view(),name="tasks"),
+    path("task/<int:pk>/",view=TaskDetail.as_view(),name="task"),
+    path("task-create/",view=TaskCreate.as_view(),name="task-create"),
+
 ]
