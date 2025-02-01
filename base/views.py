0980799c6adccc78b3ef67868142412ccef3fa14
Joshwa Sanz <joshwasanz@gmail.com>
2025-02-01 09:00:40 -0500

    delete feature implemented

--- base/views.py
+++ base/views.py
@@ -26,3 +26,8 @@ class TaskUpdate(UpdateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
